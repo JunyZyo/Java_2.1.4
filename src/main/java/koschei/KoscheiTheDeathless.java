42a79ec0fd6e1651885642f0db81431e76f4a6b4
JunyZyo <junior0101025@mail.ru>
2026-03-29 00:54:32 +0300

    2

--- src/main/java/koschei/KoscheiTheDeathless.java
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -1,5 +1,6 @@
 package koschei;
 
+import koschei.models.Island2;
 import koschei.models.Ocean1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 public class KoscheiTheDeathless {
 
     private Ocean1 ocean;
+//    private Island2 island;
 
     public String getRulesByDeth() {
         return "На свете есть океан , " + ocean.toString();
@@ -17,4 +19,6 @@ public class KoscheiTheDeathless {
     public void setOcean(Ocean1 ocean) {
         this.ocean = ocean;
     }
+//    @Autowired
+//    public void setIsland(Island2 island) { this.island = island;}
 }
