3069a610e23b86d63a29d17a58459a20c489b0c4
JunyZyo <junior0101025@mail.ru>
2026-03-30 23:32:59 +0300

    4

--- src/main/java/koschei/KoscheiTheDeathless.java
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -1,6 +1,6 @@
 package koschei;
 
-import koschei.models.Ocean1;
+import koschei.models.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +8,13 @@ import org.springframework.stereotype.Component;
 public class KoscheiTheDeathless {
 
     private Ocean1 ocean;
+    private Island2 island;
+    private Wood3 wood;
+    private Rabbit4 rabbit;
+    private Duck5 duck;
+    private Egg6 egg;
+    private Needle7 needle;
+    private Deth8 deth;
 
     public String getRulesByDeth() {
         return "На свете есть океан , " + ocean.toString();
@@ -16,5 +23,33 @@ public class KoscheiTheDeathless {
     @Autowired
     public void setOcean(Ocean1 ocean) {
         this.ocean = ocean;
+    }
+    @Autowired
+    public void setIsland(Island2 island) {
+        this.island = island;
+    }
+    @Autowired
+    public void setWood(Wood3 wood) {
+        this.wood = wood;
+    }
+    @Autowired
+    public void setRabbit(Rabbit4 rabbit) {
+        this.rabbit = rabbit;
+    }
+    @Autowired
+    public void setDuck(Duck5 duck) {
+        this.duck = duck;
+    }
+    @Autowired
+    public void setEgg(Egg6 egg) {
+        this.egg = egg;
+    }
+    @Autowired
+    public void setNeedle(Needle7 needle) {
+        this.needle = needle;
+    }
+    @Autowired
+    public void setDeth(Deth8 deth) {
+        this.deth = deth;
     }
 }
